8f73a6bee5b77a36d7b3a0d4b83adac9e85c99f6
frame88 <fleuzzi97@gmail.com>
2021-11-22 18:19:46 +0100

    complete

---
+++ main.js
@@ -0,0 +1,12 @@
+const hamburg = document.querySelector(".header-right > a");
+const nav = document.querySelector(".hamburger-menu");
+const hamburgClose = document.querySelector(".close");
+
+hamburg.addEventListener('click', function() {
+    hamburg.classList.toggle("active");
+    nav.classList.toggle("active");
+})
+
+hamburgClose.addEventListener('click', function() {
+    nav.classList.remove("active");
+}) 
