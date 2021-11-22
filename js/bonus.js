d41badb0618d27b8b00daf94f5e5a846c89378ed
frame88 <fleuzzi97@gmail.com>
2021-11-22 19:16:50 +0100

    bonus.js

---
+++ js/bonus.js
@@ -0,0 +1,22 @@
+const hamburgz = document.querySelector(".header-right > a");
+const navz = document.querySelector(".hamburger-menu");
+const hamburgClosez = document.querySelector(".close");
+
+hamburgz.addEventListener('click', function () {
+    hamburgz.classList.toggle("active");
+    navz.classList.toggle("active");
+})
+
+hamburgClosez.addEventListener('click', function () {
+    navz.classList.remove("active");
+})
+
+const cicimiaoz = document.getElementById("barre");
+cicimiaoz.style.color = "red";
+
+const logozz = document.getElementById("logo");
+logozz.style.color = "yellow";
+logozz.style.backgroundColor = "red";
+
+const divz = document.querySelector("div.header-left");
+divz.append('mio testo');
--- js/main.js
+++ js/main.js
@@ -11,12 +11,3 @@ hamburgClose.addEventListener('click', function() {
     nav.classList.remove("active");
 }) 
 
-const cicimiao = document.getElementById("barre");
-cicimiao.style.color = "red";
-
-const logoz = document.getElementById("logo");
-logoz.style.color = "yellow";
-logoz.style.backgroundColor = "red";
-
-const div = document.querySelector("div.header-left");
-div.append('mio testo');
